336998f323e074d44c9f4d31844758fdc63f0b92
Minh Nguyen <nnminh742@gmail.com>
2025-03-24 16:35:56 +0700

    import swiper

---
+++ src/declarations.d.ts
@@ -0,0 +1,24 @@
+declare module 'swiper/css' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/pagination' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/navigation' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/autoplay' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/effect-fade' {
+  const content: any;
+  export default content;
+}
