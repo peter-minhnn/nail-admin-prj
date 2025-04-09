ac58748954f443431452758f40f7537d120adb8a
HaiCK <>
2025-04-09 13:59:54 +0700

    - change formData to json

--- src/services/guest/contact.service.ts
+++ src/services/guest/contact.service.ts
@@ -1,24 +1,25 @@
 import { apiGuestRoutes } from '@/config/guest.api.route'
-import { ContactDataType } from '@/entities/(guest)/contact'
 import { BaseResponseType } from '@/types'
 import { useGlobalAxios } from '@/hooks/use-axios'
 import { handleApiCatchResponse, handleApiResponse } from '../api.service'
+import { ContactDataType } from '@/features/(guest)/contact/data/shema'
 
 export const sendRequests = async (data: ContactDataType) => {
-  const formData = new FormData()
-  formData.append('firstName', data.name)
-  formData.append('lastName', data.name)
-  formData.append('email', data.email)
-  formData.append('phoneNumber', data.phone)
-  formData.append('subject', data.topic)
-  formData.append('content', data.content ?? '')
-
+  const body = {
+    'firstName': data.name,
+    'lastName': data.name,
+    "address": data.address,
+    "email": data.email,
+    "phoneNumber": data.phone,
+    "subject": data.topic,
+    "content": data.content
+  }
   try {
-    const response = await useGlobalAxios.postFormData<
+    const response = await useGlobalAxios.post<
       null,
       BaseResponseType,
-      FormData
-    >(apiGuestRoutes.contact.general, formData)
+      object
+    >(apiGuestRoutes.contact.general, body)
     return handleApiResponse<any>(response)
   } catch (e) {
     return handleApiCatchResponse<any>(e)
