47b8fc7ffdca44ef1f32277b9dde7e720ff2a188
HaiCK <>
2025-04-12 22:47:17 +0700

    - auto down line for activity

--- src/features/(guest)/home/components/activity/home-activities.tsx
+++ src/features/(guest)/home/components/activity/home-activities.tsx
@@ -40,13 +40,13 @@ export default function HomeActivities() {
   if ((dataSource.data ?? []).length == 0) return <div />
   return (
     <div className='h-fit grid-cols-1 grid-rows-2 gap-8 py-32 pt-0 sm:grid md:grid-cols-2 md:py-32'>
-      <div className='flex w-full flex-row items-center justify-between pb-7 pt-24 sm:flex-col sm:items-start sm:justify-start md:pb-24 md:pt-0'>
-        <h3
-          className={`w-max text-start font-philosopher text-5xl font-normal sm:pb-[68px] md:text-7xl`}
-        >
-          {intl.formatMessage({ id: 'guest.common.activity' })}
-        </h3>
-        <div className='flex items-end justify-start'>
+      <div className='flex w-full flex-row items-center  justify-between pb-7 sm:flex-col sm:items-start sm:justify-start md:pb-24 md:pt-0'>
+        <p className={`philosopher-regular flex text-5xl sm:mb-16 sm:text-7xl`}>
+          {intl.formatMessage({
+            id: 'guest.common.activity',
+          })}
+        </p>
+        <div className='roboto-regular inline-block w-max items-end justify-end sm:hidden'>
           <Link
             to={menuRoutes.activity}
             onClick={() => window.scrollTo(0, 0)}
