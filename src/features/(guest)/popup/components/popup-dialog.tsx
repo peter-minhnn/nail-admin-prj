f6153b793a8f0a917b7130dc21b15172112593fe
HaiCK <>
2025-05-23 10:33:13 +0700

    update : disable click content popup

--- src/features/(guest)/popup/components/popup-dialog.tsx
+++ src/features/(guest)/popup/components/popup-dialog.tsx
@@ -38,60 +38,66 @@ export default function PopupDialog() {
 
   if (popupsData == null) return null
   return (
-    <div className='fixed inset-0 z-[99999] flex items-center justify-center  bg-black/10' onClick={(e) => {
-      e.stopPropagation()
-      handleClose()
-    }}>
-      <div
-        className=' rounded-3xl overflow-hidden  w-[90%] max-w-[600px] max-h-[90%] justify-center items-center flex'>
-        <div className='flex h-fit w-full flex-col p-[2px]  bg-white gap-1 overflow-hidden rounded-3xl items-center justify-center'>
-          <div className='rounded-3xl overflow-hidden w-full h-f'>
-            <div className='flex flex-1'>
-              {popupsData.type === 'image' ? <PopupImage popup={popupsData} /> : <PopupContent popup={popupsData} />}
-            </div>
-            <div className='flex w-full h-[60px] flex-row'>
-              <div className='flex flex-1 items-center gap-2 bg-gray-50 px-3'>
-                <label className='flex cursor-pointer items-center gap-2'>
-                  <input
-                    id='doNotShow'
-                    type='checkbox'
-                    checked={doNotShowToday}
-                    onChange={handleSkipShowToday}
-                    className='peer hidden'
-                  />
-                  <div className='border-grey-500 flex h-5 w-5 items-center justify-center rounded-sm border-2 peer-checked:border-orange-500 peer-checked:bg-orange-500'>
-                    <svg
-                      className='h-5 w-5 text-white peer-checked:block'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      stroke='currentColor'
-                      strokeWidth='3'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    >
-                      <polyline points='20 6 9 17 4 12' />
-                    </svg>
-                  </div>
-                  <span className='font-roboto text-xs md:text-xs lg:text-base'>
-                    {intl.formatMessage({ id: 'popup.publish.skipToDay' })}
-                  </span>
-                </label>
-              </div>
 
-              <button
-                className='flex w-[60px] items-center justify-center bg-orange-500 hover:bg-orange-700'
-                onClick={handleClose}
-              >
-                <img
-                  src='/images/svg/ic-close.svg'
-                  alt='btnClose'
-                  className='h-fit w-fit'
-                />
-              </button>
+    <div className='fixed inset-0 z-[99999] flex items-center justify-center  bg-black/10' >
+      <button onClick={(e) => {
+        e.stopPropagation()
+        handleClose()
+      }} className='flex h-full w-full  items-center justify-center '>
+        <button
+          onClick={(e) => {
+            e.stopPropagation()
+          }} className=' rounded-3xl overflow-hidden  w-[90%] max-w-[600px] max-h-[90%] justify-center items-center flex'>
+          <div className='flex h-fit w-full flex-col p-[2px]  bg-white gap-1 overflow-hidden rounded-3xl items-center justify-center'>
+            <div className='rounded-3xl overflow-hidden w-full h-f'>
+              <div className='flex flex-1'>
+                {popupsData.type === 'image' ? <PopupImage popup={popupsData} /> : <PopupContent popup={popupsData} />}
+              </div>
+              <div className='flex w-full h-[60px] flex-row'>
+                <div className='flex flex-1 items-center gap-2 bg-gray-50 px-3'>
+                  <label className='flex cursor-pointer items-center gap-2'>
+                    <input
+                      id='doNotShow'
+                      type='checkbox'
+                      checked={doNotShowToday}
+                      onChange={handleSkipShowToday}
+                      className='peer hidden'
+                    />
+                    <div className='border-grey-500 flex h-5 w-5 items-center justify-center rounded-sm border-2 peer-checked:border-orange-500 peer-checked:bg-orange-500'>
+                      <svg
+                        className='h-5 w-5 text-white peer-checked:block'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        stroke='currentColor'
+                        strokeWidth='3'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                      >
+                        <polyline points='20 6 9 17 4 12' />
+                      </svg>
+                    </div>
+                    <span className='font-roboto text-xs md:text-xs lg:text-base'>
+                      {intl.formatMessage({ id: 'popup.publish.skipToDay' })}
+                    </span>
+                  </label>
+                </div>
+
+                <button
+                  className='flex w-[60px] items-center justify-center bg-orange-500 hover:bg-orange-700'
+                  onClick={handleClose}
+                >
+                  <img
+                    src='/images/svg/ic-close.svg'
+                    alt='btnClose'
+                    className='h-fit w-fit'
+                  />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </button>
+      </button >
+    </div >
+
   )
 }
